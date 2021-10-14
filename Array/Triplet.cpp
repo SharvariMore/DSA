dde2e64cd7c456ff330f976b289cbb88df0a0837
Sharvari More <83008601+SharvariMore@users.noreply.github.com>
2021-10-15 01:02:28 +0530

    Updating comments

--- Array/Triplet.cpp
+++ Array/Triplet.cpp
@@ -8,14 +8,14 @@ vector<vector <int>> TripletSum(vector <int> arr, int targetSum)
     int n = arr.size();
     vector<vector <int>> result;
 
-    sort(arr.begin(),arr.end());
+    sort(arr.begin(),arr.end());    //sort array
 
-    for(int i=0;i<=n-3;i++)
+    for(int i=0;i<=n-3;i++)         //point to current element & pair sum of remaining part
     {
-        int j = i + 1;
-        int k = n - 1;
+        int j = i + 1;              //move forward
+        int k = n - 1;              //move backward
 
-    while(j<k)
+    while(j<k)                    //avoid overlapping
      {
         int currentSum = arr[i];
         currentSum += arr[j];
