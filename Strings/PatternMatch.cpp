cb9d2ddad73af98618f701629f6c07623cff3cd0
Sharvari More <83008601+SharvariMore@users.noreply.github.com>
2021-10-20 03:22:29 +0530

    Return index if string matches given pattern

---
+++ Strings/PatternMatch.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+
+void search(char* pat, char* txt)
+{
+    int M = strlen(pat);
+    int N = strlen(txt);
+
+    for (int i = 0; i <= N-M; i++)
+    {
+       int j;
+       for(j=0;j<M;j++)
+        if(txt[i+j]!=pat[j])
+        break;
+
+        if(j==M)
+            cout<<"\n Element found at index : "<<i;
+    }
+    
+}
+
+int main()
+{
+    char txt[]="AABAACAADAABAAABAA";
+    char pat[]="AABA";
+    search(pat,txt);
+    return 0;
+}
