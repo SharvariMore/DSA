08e7e6f4173f19b845f8ce9cb13ffe966b5199eb
Sharvari More <83008601+SharvariMore@users.noreply.github.com>
2021-10-23 01:35:26 +0530

    Sort names in ascending order

---
+++ Strings/SortNamesAsc.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main()
+{
+    int i, j, n;
+    char str[100][100], s[100];
+
+    cout<<"\n Enter no. of names : ";
+    cin>>n;
+
+    cout<<"\n Enter names : ";
+    for (i = 0; i < n; i++)
+    {
+        cin>>str[i];
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = i+1; j < n; j++)
+        {
+            if (strcmp(str[i],str[j])>0)
+            {
+                strcpy(s,str[i]);
+                strcpy(str[i],str[j]);
+                strcpy(str[j],s);
+            }
+            
+        }
+        
+    }
+
+    cout<<"\n The names in sorted order are : ";
+    for (i = 0; i < n; i++)
+    {
+        cout<<"\n"<<str[i];
+    }
+    
+    return 0;
+    
+}
