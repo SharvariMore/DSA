882fd335ac800702ce0606d49feb5f42d48396be
Sharvari More <83008601+SharvariMore@users.noreply.github.com>
2021-10-26 00:52:19 +0530

    Replace 20% for spaces

--- Strings/Replace20.cpp
+++ Strings/Replace20.cpp
@@ -1,2 +1,48 @@
 #include<iostream>
-using namepace std;
+#include<cstring>
+using namespace std;
+
+void replace20(char *str)
+{
+    int spaces = 0;
+    for (int i = 0; str[i]!='\0'; i++)
+    {
+        if (str[i]==' ')      //if space found between 2 words search for next spaces too
+        {
+            spaces += 1;
+        }
+        
+    }
+
+    int index = strlen(str) + 2*spaces;     //return length of current string + spaces*2
+    str[index] = '\0';                      
+
+    for (int i = strlen(str)-1; i >= 0; i--)    //go to end of string
+    {
+        if (str[i]==' ')               //if space found replace with 20% between 2 words for the space
+        {
+            str[index - 1] = '0';     
+            str[index - 2] = '2';      
+            str[index - 3] = '%';      
+            index = index - 3;
+        }
+        else
+        {
+            str[index - 1] = str[i];   //if space not found move to next word & decrement index
+            index--;
+        }
+        
+    }
+    
+}
+
+int main()
+{
+    char input[10000];
+    cin.getline(input,1000);
+    replace20(input);
+    cout<<input<<endl;
+
+    return 0;
+
+}
